8be922ed4f419fc8f2f1a6030d4dd9674b128e29
DanielPizanoo <danielhp74@gmail.com>
2020-11-22 23:13:47 -0600

    Up code

---
+++ Client.py
@@ -0,0 +1,17 @@
+import socket
+
+msgFromClient = "Hello UDP Server"
+byteToSend = str.encode(msgFromClient)
+serverAddressPort = ("127.0.0.1", 20001)
+bufferSize = 1024
+
+#Crear un socket UDP en el lado del cliente
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+#Enviar al servidor usando el socket UDP creado
+UDPClientSocket.sendto(byteToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+msg = "Message from Server {}".format(msgFromServer[0])
+print(msg)
---
+++ Server.py
@@ -0,0 +1,30 @@
+import socket
+
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
+
+msgFromServer = "Hello UDP Client"
+byteToSend = str.encode(msgFromServer)
+
+#Crea un socket de datagrama
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+#Vincular a direccion IP
+UDPServerSocket.bind((localIP, localPort))
+
+print("UDP server up and listening")
+
+#Escuchando para los datagramas entrantes
+while(True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+    clientMsg = "Message from clien:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+
+    print(clientMsg)
+    print(clientIP)
+
+    #Enviando respuesta al cliente
+    UDPServerSocket.sendto(byteToSend, address)
